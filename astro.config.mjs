b2412ad6d2f1365033777647b1df3d43af44d2c2
mwww dev <115621609+mwww@users.noreply.github.com>
2023-06-29 01:48:58 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://mwww.github.io/',
-    base: '/my-astro_portfolio1',
+    base: '/astro_portfolio1',
 });
